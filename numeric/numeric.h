8cd760309b4d8d7e5b59a08f9b56b1c7b6adad59
Константин Томилов <tomilov.k@7gis.ru>
2025-03-13 15:14:53 +0500

    добавил циклические сдвиги и определение разрядности

--- numeric/numeric.h
+++ numeric/numeric.h
@@ -4,6 +4,8 @@
 
 #define NUM_VALUE_MAX 0x7FFFFFFF // 2 147 483 647
 #define NUM_VALUE_MIN 0x80000000 // -2 147 483 648
+#define BIT_DEPTH 32
+
 
 abs(num);
 coerce(&num, rangeMin, rangeMax);
@@ -15,5 +17,7 @@ pow(num, p);
 rnd(atLeast, noGreater);
 setBit(byteValue, bit, val);
 countUsedCells(size, cellSize);
+rol(value, bits);
+ror(value, bits);
 
 #endif // NUMERIC_H
