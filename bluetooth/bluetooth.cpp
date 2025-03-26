e3f25ecae38bf55823d8b6b59b1219caa90b1d25
Константин Томилов <tomilov.k@7gis.ru>
2025-03-26 17:58:10 +0500

    добавил определения, получение companyId, рефакторинг

--- bluetooth/bluetooth.cpp
+++ bluetooth/bluetooth.cpp
@@ -19,17 +19,18 @@ bluetoothGetAdStruct(const msg[BTMSG], pos, adStruct[BLUETOOTH_ADSTRUCT])
     if ((pos + BLUETOOTH_ADSTRUCT_SIZE_MIN) > msgDataSize)
         return 0;
     
-    adStruct.dataSize = msg.data{pos++} - 1;
-    if ((insertArrayStr(adStruct.data, 0, BLUETOOTH_ADSTRUCT_DATA_SIZE_MAX, msg.data, msgDataSize, pos + 1) < adStruct.dataSize)
-        || (adStruct.dataSize <= 0))
+    adStruct.dataSize = msg.data{pos + BLUETOOTH_ADSTRUCT_LEN_POS} - BLUETOOTH_ADSTRUCT_LEN_SIZE;
+    if ((adStruct.dataSize < BLUETOOTH_ADSTRUCT_SIZE_MIN)
+        || (insertArrayStr(adStruct.data, 0, BLUETOOTH_ADSTRUCT_DATA_SIZE_MAX, msg.data, msgDataSize, pos + BLUETOOTH_ADSTRUCT_PAYLOAD_POS)
+            != adStruct.dataSize))
         return 0;
 
-    adStruct.type = msg.data{pos};
+    adStruct.type = msg.data{pos + BLUETOOTH_ADSTRUCT_TYPE_POS};
     return BLUETOOTH_ADSTRUCT_SIZE_MIN + adStruct.dataSize;
 }
 
 //! Получить имя из AD Struct
-//! \param[in] adStruct AD Struct
+//! \param[in] adStruct AD Struct с Local Name
 //! \param[in] nameMaxSize максимальная длина для имени
 //! \param[out] name полученное имя
 //! \param[out] nameSize длина полученного имени
@@ -47,6 +48,15 @@ bluetoothGetLocalName(const adStruct[BLUETOOTH_ADSTRUCT], nameMaxSize, name{}, &
 bluetoothGetRssi(const msg[BTMSG])
 {
     return msg.rssi | BLUETOOTH_RSSI_MASK;
+}
+
+//! Получить Company Id из AD Struct
+//! \param[in] adStruct AD Struct c Manufacturer Specific Data
+//! \param[out] result полученный идентификатор
+//! \return true - успешно, false - ошибка
+bluetoothGetCompanyId(const adStruct[BLUETOOTH_ADSTRUCT], &result)
+{
+    return (adStruct.type == BLUETOOTH_ADTYPE_MFRSPECDATA) && array2num16leUnSign(adStruct.data, BLUETOOTH_MFRSPECDATA_COMP_ID_POS, adStruct.dataSize, result);
 }
 
 #endif
