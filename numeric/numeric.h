1e5b881d3372c91c25919e34ffe38cfbd7cfde0e
Константин Томилов <tomilov.k@7gis.ru>
2025-04-10 16:14:05 +0500

    добавил макросы min, max, COUNT_USED_CELLS

--- numeric/numeric.h
+++ numeric/numeric.h
@@ -6,6 +6,17 @@
 #define NUM_VALUE_MIN 0x80000000 // -2 147 483 648
 #define BIT_DEPTH 32
 
+#ifndef MIN
+#define MIN(%1,%2) ((%1) < (%2) ? (%1) : (%2)) // при многих вложенных таких же - ЗАВИСАЕТ КОМПИЛЯЦИЯ
+#endif
+
+#ifndef MAX
+#define MAX(%1,%2) ((%1) > (%2) ? (%1) : (%2)) // при многих вложенных таких же - ЗАВИСАЕТ КОМПИЛЯЦИЯ
+#endif
+
+#ifndef COUNT_USED_CELLS
+#define COUNT_USED_CELLS(%1,%2) ((%1) / (%2) + (((%1) % (%2)) != 0))
+#endif
 
 abs(num);
 coerce(&num, rangeMin, rangeMax);
