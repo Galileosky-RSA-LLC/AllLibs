3706b779f7511d290bc750682bcd37e6f405b17e
Константин Томилов <tomilov.k@7gis.ru>
2025-04-14 10:18:48 +0500

    изменил replaceStr на strncpy

--- string/string.cpp
+++ string/string.cpp
@@ -66,20 +66,21 @@ strLen(const str{}, strLength = 0, start = 0)
     return i - start;
 }
 
-//! Замена одной подстроки на другую
-//! \param[out] strDest массив строки-приемника
-//! \param[in] strDestPos позиция начала вставки в строку-приемник
-//! \param[in] strDestLength длина массива строки-приемника
-//! \param[in] strSource массив строки-источника
-//! \param[in] strSourcePos начальная позиция в строке-источнике
-replaceStr(strDest{}, strDestPos, strDestLength, const strSource{}, strSourcePos = 0)
+//! Копирование одной подстроки в другую с ее завершением (если позволяет длина)
+//! \param[out] dest массив строки-приемника
+//! \param[in] destPos позиция начала вставки в строку-приемник
+//! \param[in] destLength длина массива строки-приемника
+//! \param[in] source массив строки-источника
+//! \param[in] sourcePos начальная позиция в строке-источнике
+//! \param[in] sourceLength длина массива строки-источника (если <=0, то будет вычислена)
+strncpy(dest{}, destPos, destLength, const source{}, sourcePos = 0, sourceLength = 0)
 {
-    if (strDestPos < 0)
-        strDestPos = 0;
+    if (destPos < 0)
+        destPos = 0;
 
-    new endPos = strDestPos + insertArrayStr(strDest, strDestPos, strDestLength, strSource, strLen(strSource), strSourcePos);
-	if ((endPos >= 0) && (endPos < strDestLength))
-        strDest{endPos} = 0;
+    new endPos = destPos + insertArrayStr(dest, destPos, destLength, source, strLen(source, sourceLength), sourcePos);
+	if ((endPos >= 0) && (endPos < destLength))
+        dest{endPos} = 0;
 }
 
 //! Преобразовать нечитаемые символы в пробелы
