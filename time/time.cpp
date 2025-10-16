027df328fe075946869cfc223c6ca4ee031a4370
Константин Томилов <tomilov.k@7gis.ru>
2025-10-16 13:12:23 +0500

    переименовал переменную для устранения затенения и для читаемости, добавил скобки для читаемости, упростил вычисление условий

--- time/time.cpp
+++ time/time.cpp
@@ -34,15 +34,15 @@ stock waitFrom(fixedUptime, timer)
 
 stock unixTime2dateTime(time, &year, &month, &day, &hour, &minute, &second)
 {
-    new i = 0;
+    new yearsFromStart = 0;
     new thisYearSeconds = NONLEAP_YEAR_SECONDS;
     while (time >= thisYearSeconds)
     {
-        i++;
+        yearsFromStart++;
         time -= thisYearSeconds;
-        thisYearSeconds = NONLEAP_YEAR_SECONDS + ((i % 4 == 2) ? SECONDS_PER_DAY : 0);
+        thisYearSeconds = NONLEAP_YEAR_SECONDS + (((yearsFromStart % 4) == 2) ? SECONDS_PER_DAY : 0);
     }
-    year = UNIX_EPOCH_YEAR_START + i;
+    year = UNIX_EPOCH_YEAR_START + yearsFromStart;
     new monthDays[] = [JANUARY_DAYS, FEBRUARY_NONLEAP_DAYS, MARCH_DAYS, APRIL_DAYS, MAY_DAYS, JUNE_DAYS, JULY_DAYS, AUGUST_DAYS, SEPTEMBER_DAYS, OCTOBER_DAYS,
                         NOVEMBER_DAYS, DECEMBER_DAYS];
     if (isLeapYear(year))
@@ -50,9 +50,9 @@ stock unixTime2dateTime(time, &year, &month, &day, &hour, &minute, &second)
 
     new daysElapsed = time / SECONDS_PER_DAY;
     new day1 = daysElapsed;
-    for (new i = 0; i <= sizeof monthDays - 1; i++)
+    for (new i = 0; i < sizeof(monthDays); i++)
     {
-        if (day1 <= (monthDays[i] - 1))
+        if (day1 < monthDays[i])
         {
             month = i + 1;
             day = day1 + 1;
@@ -60,11 +60,11 @@ stock unixTime2dateTime(time, &year, &month, &day, &hour, &minute, &second)
         }
         day1 -= monthDays[i];
     }
-    new secondsFromMidnight = time - daysElapsed * SECONDS_PER_DAY;
+    new secondsFromMidnight = time - (daysElapsed * SECONDS_PER_DAY);
     hour = secondsFromMidnight / SECONDS_PER_HOUR;
-    new secondsFromHour = secondsFromMidnight - hour * SECONDS_PER_HOUR;
+    new secondsFromHour = secondsFromMidnight - (hour * SECONDS_PER_HOUR);
     minute = secondsFromHour / SECONDS_PER_MINUTE;
-    second = secondsFromHour - minute * SECONDS_PER_MINUTE;
+    second = secondsFromHour -( minute * SECONDS_PER_MINUTE);
 }
 
 stock uptimeLess(uptime1, uptime2)
