49be5e11c87c3a60c2c8c88773ba49dd8ab55caa
Константин Томилов <tomilov.k@7gis.ru>
2025-10-22 11:43:41 +0500

    исправил размеры массивов, чтобы не вырождались в переменную, когда 1 настройка, обновил lic

--- setting/setting.h
+++ setting/setting.h
@@ -13,22 +13,26 @@
 
 #define SETTING_CONF_CMD_WAIT_MS 5000 //!< время ожидания конфигурационных команд при отсутствии доступа к файловой системе
 
+#define SETTING_PARAM_SIZE CELL_BYTES
 #ifndef SETTING_PARAMS_AMOUNT
 #define SETTING_PARAMS_AMOUNT 0 //!< общее количество сохраняемых параметров, определить на верхнем уровне
 #endif
-if (SETTING_PARAMS_AMOUNT < 1)
+#if (SETTING_PARAMS_AMOUNT < 1)
 #error "SETTING_PARAMS_AMOUNT wrong"
+#elseif (SETTING_PARAMS_AMOUNT == 1)
+#define SETTING_PARAMS_ADD_AMOUNT 2
+#else
+#define SETTING_PARAMS_ADD_AMOUNT SETTING_PARAMS_AMOUNT
 #endif
+#define SETTING_BUF_SIZE (SETTING_PARAMS_AMOUNT * SETTING_PARAM_SIZE)
+
 #define SETTING_FILENAME_SIZE_MAX FILE_FULL_PATH_SIZE_MAX
 #define SETTING_FILENAME_SIZE_MAX_WITH_0 (SETTING_FILENAME_SIZE_MAX + 1)
 
-#define SETTING_PARAM_SIZE CELL_BYTES
-#define SETTING_BUF_SIZE (SETTING_PARAMS_AMOUNT * SETTING_PARAM_SIZE)
-
 #define SETTING_DATA [\
     .fileName{SETTING_FILENAME_SIZE_MAX_WITH_0},\
-    .varAddr{SETTING_BUF_SIZE},\
-    .oldValue{SETTING_BUF_SIZE},\
+    .varAddr[SETTING_PARAMS_ADD_AMOUNT],\
+    .oldValue[SETTING_PARAMS_ADD_AMOUNT],\
 ]
 
 #define SETTING_SINGLE_DATA [\
