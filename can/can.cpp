8b212a2620a83e2d7c478ac47d152aef171368bb
Константин Томилов <tomilov.k@7gis.ru>
2026-03-06 16:25:06 +0500

    can: doxygen, добавил forward и stock, добавил недостающие теги bool, добавил порт 2, исправил вывод can сообщения с учетом реальной длины данных

--- can/can.cpp
+++ can/can.cpp
@@ -1,6 +1,10 @@
-#if !defined CAN_LIB
+//! @file
+//! @brief Реализация библиотеки работы с CAN
+
+#if defined CAN_LIB
+#endinput
+#endif
 #define CAN_LIB
-// Библиотека работы с CAN
 
 #include "can.h"
 #include "..\string\string.h"
@@ -8,63 +12,46 @@
 #include "..\array\array.h"
 #include "..\array\array.cpp"
 
-//! Вывод в диагностику сообщения CAN
-//! \param[in] mes выводимое сообщение
-//! \param[in] portNum номер порта
-//! \param[in] send признак отправки сообщения: !0 - отправка, 0 - прием
-canDiagMessage(const mes[CANMSG], portNum, send)
+stock canDiagMessage(const mes[CANMSG], portNum, bool:isSend)
 {
-    Diagnostics("CAN%d %s.id=0x%X,ds=%d:0x%X %X", portNum, send ? "s" : "r", mes.id, mes.dataSize, mes.data[0], mes.data[1]);
+    clearArrayStr(mes.data, CAN_DATASIZE_GENERAL_MAX, mes.dataSize);
+    Diagnostics("CAN%d %s.id%d=%Xh,ds=%d:%X %Xh", portNum, isSend ? "s" : "r", mes.idType, mes.id, mes.dataSize, mes.dataSize > 0 ? mes.data[0] : 0,
+                mes.dataSize > 4 ? mes.data[1] : 0);
 }
 
-//! Вывод в диагностику отправляемого сообщения CAN
-//! \param[in] mes выводимое сообщение
-//! \param[in] portNum номер порта
-canDiagSentMessage(const mes[CANMSG], portNum)
+stock canDiagSentMessage(const mes[CANMSG], portNum)
 {
     canDiagMessage(mes, portNum, true);
 }
 
-//! Вывод в диагностику принятого сообщения CAN
-//! \param[in] mes выводимое сообщение
-//! \param[in] portNum номер порта
-canDiagReceivedMessage(const mes[CANMSG], portNum)
+stock canDiagReceivedMessage(const mes[CANMSG], portNum)
 {
     canDiagMessage(mes, portNum, false);
 }
 
-//! Получить установленные параметры для CAN порта
-//! \param[in] portIndex индекс порта
-//! \param[out] mode установленный режим CAN_MODE_
-//! \param[out] baudRate установленная скорость, бит/с
-//! \return true - успешно, false - ошибка
-getCanRegime(portIndex, &mode, &baudRate)
+stock bool:getCanRegime(portNum, &mode, &baudRate)
 {
-    if (portIndex < CAN_PORT_NUM_MIN)
-        portIndex = CAN_PORT_NUM_MIN;
+    if (portNum < CAN_PORT_NUM_MIN)
+        portNum = CAN_PORT_NUM_MIN;
     
-    if (portIndex > CAN_PORT_NUM_MAX)
-        portIndex = CAN_PORT_NUM_MAX;
+    if (portNum > CAN_PORT_NUM_MAX)
+        portNum = CAN_PORT_NUM_MAX;
 
-    ExecCommand(portIndex == 0 ? "CANREGIME" : (portIndex == 1 ? "CAN1REGIME" : "CAN2REGIME"));
+    ExecCommand(portNum == 0 ? "CANREGIME" : (portNum == 1 ? "CAN1REGIME" : "CAN2REGIME"));
     const answerMaxSize = 80;
     new answer{answerMaxSize};
     GetBinaryDataFromCommand(answer, answerMaxSize); //типа "CAN1REGIME:Mode=11,BaudRate=1000000,TimeOut=2000,DoNotCleanAfterTimeOut=1;"
     new len = strLen(answer, answerMaxSize);
     toLowerCase(answer, len);
-    const modeStrSize = 5;
     new const modeStr{} = "mode=";
     new idx = searchStr(answer, modeStr);
-    if ((idx < 0) || !atoi(answer, mode, len, idx + modeStrSize))
+    if ((idx < 0) || !atoi(answer, mode, len, idx + strLen(modeStr)))
         return false;
 
-    const baudRateStrSize = 9;
     new const baudRateStr{} = "baudrate=";
     idx = searchStr(answer, baudRateStr);
     if (idx < 0)
         return false;
     
-    return atoi(answer, baudRate, len, idx + baudRateStrSize);
+    return atoi(answer, baudRate, len, idx + strLen(baudRateStr));
 }
-
-#endif
