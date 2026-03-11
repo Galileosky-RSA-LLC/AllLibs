49cb265a82e1bcbb2e6d593282d9d3e375ffdf26
Константин Томилов <tomilov.k@7gis.ru>
2026-03-11 17:55:29 +0500

    doxygen, убрал лишний пустой файл, форматирование

--- jpeg/exif/exif.cpp
+++
@@ -1,5 +0,0 @@
-#if !defined EXIF_LIB
-#define EXIF_LIB
-// Библиотека работы с EXIF изображениями
-
-#endif
--- jpeg/exif/exif.h
+++ jpeg/exif/exif.h
@@ -1,6 +1,10 @@
-#if !defined EXIF_H
+//! @file
+//! @brief Заголовок библиотеки работы с EXIF изображениями
+
+#if defined EXIF_H
+#endinput
+#endif
 #define EXIF_H
-// заголовок библиотеки работы с EXIF изображениями
 
 #include "..\jpeg.h"
 
@@ -9,5 +13,3 @@ stock const EXIF_SEGMENT_MARKER{} = {JPEG_SEGMENT_MARKER_PREFIX, EXIF_SEGMENT_ID
 #define EXIF_SEGMENT_MARKER_SIZE JPEG_SEGMENT_MARKER_SIZE
 #define EXIF_LEN_POS JPEG_SEGMENT_APP_LEN_POS
 #define EXIF_LEN_SIZE JPEG_SEGMENT_APP_LEN_SIZE
-
-#endif
