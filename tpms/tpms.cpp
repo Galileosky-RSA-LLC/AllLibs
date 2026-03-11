706a461a712e41c94893d93f154bcd14c0b98f06
Константин Томилов <tomilov.k@7gis.ru>
2026-03-11 10:35:51 +0500

    doxygen, добавил forward и stock, уточнил описания, перераспределил определения, вынес повторяющиеся выражения в переменные, добавил const, форматирование

--- tpms/tpms.cpp
+++ tpms/tpms.cpp
@@ -1,16 +1,55 @@
-#if !defined TPMS_LIB
+//! @file
+//! @brief Реализация библиотеки систем контроля давления в шинах
+
+#if defined TPMS_LIB
+#endinput
+#endif
 #define TPMS_LIB
-// библиотека систем контроля давления в шинах
 
 #include "tpms.h"
 #include "..\gdefines.h"
 #include "..\numeric\numeric.h"
 #include "..\numeric\numeric.cpp"
 
-//! Записать в тег колесного датчика состояние шины
-//! \param[in] wheelIdx индекс шины TPMS_WHEEL_INDEX_MIN..TPMS_WHEEL_INDEX_MAX
-//! \param[in] wheel структура данных шины
-tpmsWriteWheelTag(wheelIdx, const wheel[TPMS_WHEEL_DATA])
+//! @defgroup wheelTag Структура тега колесного датчика
+//! @{
+#define TPMS_WHEELTAG_PRES_BYTE 0
+#define TPMS_WHEELTAG_TEMP_BYTE 1
+#define TPMS_WHEELTAG_TEMP_MIN -128
+#define TPMS_WHEELTAG_TEMP_MAX 127
+#define TPMS_WHEELTAG_TEMP_MASK 0xFF
+#define TPMS_WHEELTAG_TEMP_LSHIFT (TPMS_WHEELTAG_TEMP_BYTE * 8)
+#define TPMS_WHEELTAG_FLAGS_BYTE 2
+#define TPMS_WHEELTAG_FLAGS_LSHIFT (TPMS_WHEELTAG_FLAGS_BYTE * 8)
+#define TPMS_WHEELTAG_FLAGS_ONLINE_BIT 0
+#define TPMS_WHEELTAG_FLAGS_ERROR_BIT 1
+#define TPMS_WHEELTAG_FLAGS_REASON_BIT 2
+//! @}
+
+//! @defgroup commonTag Структура общего тега со всеми шинами
+//! @{
+#define TPMS_COMTAG_WHEEL_PRES_BYTE 0
+#define TPMS_COMTAG_WHEEL_PRES_SIZE 1
+#define TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE (TPMS_COMTAG_WHEEL_PRES_BYTE + TPMS_COMTAG_WHEEL_PRES_SIZE)
+#define TPMS_COMTAG_WHEEL_TEMPFLAGS_SIZE 1
+#define TPMS_COMTAG_WHEEL_TEMP_BIT 0
+#define TPMS_COMTAG_WHEEL_TEMP_BITS 3
+#define TPMS_COMTAG_WHEEL_TEMP_MASK 0xFF
+#define TPMS_COMTAG_TEMP_MIN -40
+#define TPMS_COMTAG_TEMP_MAX 100
+#define TPMS_COMTAG_TEMP_STEP ((TPMS_COMTAG_TEMP_MAX - TPMS_COMTAG_TEMP_MIN) / ((1 << TPMS_COMTAG_WHEEL_TEMP_BITS) - 1))
+#define TPMS_COMTAG_WHEEL_ONLINE_BIT (TPMS_COMTAG_WHEEL_TEMP_BIT + TPMS_COMTAG_WHEEL_TEMP_BITS)
+#define TPMS_COMTAG_WHEEL_ERROR_BIT TPMS_COMTAG_WHEEL_ONLINE_BIT + 1
+#define TPMS_COMTAG_WHEEL_REASON_BIT TPMS_COMTAG_WHEEL_ERROR_BIT + 1
+#define TPMS_COMTAG_WHEEL_INIT_VALUE ((TPMS_REASON_PERIODIC << TPMS_COMTAG_WHEEL_REASON_BIT) \
+                                    + (1 << TPMS_COMTAG_WHEEL_ERROR_BIT) \
+                                    + (1 << TPMS_COMTAG_WHEEL_ONLINE_BIT))
+#define TPMS_COMTAG_WHEEL_SIZE (TPMS_COMTAG_WHEEL_PRES_SIZE + TPMS_COMTAG_WHEEL_TEMPFLAGS_SIZE)
+#define TPMS_COMTAG_WHEELS_SIZE (TPMS_WHEELS_MAX * TPMS_COMTAG_WHEEL_SIZE)
+#define TPMS_COMTAG_SEND_FLAG_POS (TAG_TPMS_SIZE - 1)
+//! @}
+
+stock tpmsWriteWheelTag(wheelIdx, const wheel[TPMS_WHEEL_DATA])
 {
     coerce(wheelIdx, TPMS_WHEEL_INDEX_MIN, TPMS_WHEEL_INDEX_MAX);
     new pressure = wheel.pressurePsi;
@@ -19,54 +58,51 @@ tpmsWriteWheelTag(wheelIdx, const wheel[TPMS_WHEEL_DATA])
     coerce(reason, TPMS_REASON_MIN, TPMS_REASON_MAX);
     new temperature = wheel.temperature;
     coerce(temperature, TPMS_WHEELTAG_TEMP_MIN, TPMS_WHEELTAG_TEMP_MAX);
-    new value = ((reason << TPMS_WHEELTAG_FLAGS_REASON_BIT) + ((!wheel.sensorOk << TPMS_WHEELTAG_FLAGS_ERROR_BIT) + !wheel.sensorOnline)
-                << TPMS_WHEELTAG_FLAGS_LSHIFT)
-                + ((temperature & TPMS_WHEELTAG_TEMP_MASK) << TPMS_WHEELTAG_TEMP_LSHIFT)
-                + pressure;
+    new const value = ((reason << TPMS_WHEELTAG_FLAGS_REASON_BIT)
+                        + ((!wheel.sensorOk << TPMS_WHEELTAG_FLAGS_ERROR_BIT) + !wheel.sensorOnline) << TPMS_WHEELTAG_FLAGS_LSHIFT)
+                        + ((temperature & TPMS_WHEELTAG_TEMP_MASK) << TPMS_WHEELTAG_TEMP_LSHIFT)
+                        + pressure;
     TagWriteValue(TAG_WHEEL_0 + wheelIdx, value);
 }
 
-//! Записать параметры шин в общий тег
-//! \param[in] wheels параметры шин
-tpmsWriteCommonTag(const wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA])
+stock tpmsWriteCommonTag(const wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA])
 {
     new buf{TAG_TPMS_SIZE};
     for (new i = 0; i < TPMS_WHEELS_MAX; i++)
     {
         new pressure = wheels[i].pressurePsi;
         coerce(pressure, TPMS_PRESSURE_MIN, TPMS_PRESSURE_MAX);
-        buf{i * TPMS_COMTAG_WHEEL_SIZE + TPMS_COMTAG_WHEEL_PRES_BYTE} = pressure;
+        new const wheelOffset = i * TPMS_COMTAG_WHEEL_SIZE;
+        buf{wheelOffset + TPMS_COMTAG_WHEEL_PRES_BYTE} = pressure;
         new temperature = wheels[i].temperature;
         coerce(temperature, TPMS_COMTAG_TEMP_MIN, TPMS_COMTAG_TEMP_MAX);
         temperature = (temperature - TPMS_COMTAG_TEMP_MIN) / TPMS_COMTAG_TEMP_STEP;
         new reason = wheels[i].reason;
         coerce(reason, TPMS_REASON_MIN, TPMS_REASON_MAX);
-        buf{i * TPMS_COMTAG_WHEEL_SIZE + TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE} = (reason << TPMS_COMTAG_WHEEL_REASON_BIT)
-                                                                            + (!wheels[i].sensorOk << TPMS_COMTAG_WHEEL_ERROR_BIT)
-                                                                            + (!wheels[i].sensorOnline << TPMS_COMTAG_WHEEL_ONLINE_BIT)
-                                                                            + temperature;
+        buf{wheelOffset + TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE} = (reason << TPMS_COMTAG_WHEEL_REASON_BIT)
+                                                                + (!wheels[i].sensorOk << TPMS_COMTAG_WHEEL_ERROR_BIT)
+                                                                + (!wheels[i].sensorOnline << TPMS_COMTAG_WHEEL_ONLINE_BIT)
+                                                                + temperature;
     }
     swapBuf(buf,TPMS_COMTAG_WHEELS_SIZE);
     buf{TPMS_COMTAG_SEND_FLAG_POS} = 1;
     TagWriteArray(TAG_TPMS, TAG_TPMS_SIZE, buf);
 }
 
-//! Очистить общий тег шин
-tpmsClearCommonTag()
+stock tpmsClearCommonTag()
 {
     new buf{TAG_TPMS_SIZE};
     for (new i = 0; i < TPMS_WHEELS_MAX; i++)
     {
-        buf{i * TPMS_COMTAG_WHEEL_SIZE + TPMS_COMTAG_WHEEL_PRES_BYTE} = 0;
-        buf{i * TPMS_COMTAG_WHEEL_SIZE + TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE} = TPMS_COMTAG_WHEEL_INIT_VALUE;
+        new const wheelOffset = i * TPMS_COMTAG_WHEEL_SIZE;
+        buf{wheelOffset + TPMS_COMTAG_WHEEL_PRES_BYTE} = 0;
+        buf{wheelOffset + TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE} = TPMS_COMTAG_WHEEL_INIT_VALUE;
     }
     swapBuf(buf, TPMS_COMTAG_WHEELS_SIZE);
     TagWriteArray(TAG_TPMS, TAG_TPMS_SIZE, buf);
 }
 
-//! Инициализировать параметры шин
-//! \param[inout] wheels параметры шин
-tpmsInitWheels(wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA])
+stock tpmsInitWheels(wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA])
 {
     for (new i = 0; i < TPMS_WHEELS_MAX; i++)
     {
@@ -77,5 +113,3 @@ tpmsInitWheels(wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA])
         wheels[i].reason = TPMS_REASON_PERIODIC;
     }
 }
-
-#endif
--- tpms/tpms.h
+++ tpms/tpms.h
@@ -1,40 +1,37 @@
-#if !defined TPMS_H
-#define TPMS_H
-// заголовок библиотеки систем контроля давления в шинах
+//! @file
+//! @brief Заголовок библиотеки систем контроля давления в шинах
 
-#include "..\gdefines.h"
+#if defined TPMS_H
+#endinput
+#endif
+#define TPMS_H
 
 #define TPMS_WHEELS_MAX 34
 #define TPMS_WHEEL_INDEX_MIN 0
 #define TPMS_WHEEL_INDEX_MAX (TPMS_WHEELS_MAX - 1)
 
-#define TPMS_REASON_PERIODIC 0
-#define TPMS_REASON_PRES_UNDER_10 1 // PressurePro: 10%, TPMS: 12,5%
-#define TPMS_REASON_PRES_UNDER_20 2 // PressurePro: 20%, TPMS: 25%
-#define TPMS_REASON_PRES_UNDER_50 3 // TPMS
-#define TPMS_REASON_OVERTEMP 4 // TPMS
-#define TPMS_REASON_PRES_FAST_DEC 5 // TPMS
-#define TPMS_REASON_NEWAIR_OVERPRES 6 // PressurePro: new air, TPMS: overpressure
-#define TPMS_REASON_NEWMAGNET 7 // PressurePro
-#define TPMS_REASON_MIN TPMS_REASON_PERIODIC
-#define TPMS_REASON_MAX TPMS_REASON_NEWMAGNET
-
-#define TPMS_WHEELTAG_PRES_BYTE 0
-#define TPMS_WHEELTAG_TEMP_BYTE 1
-#define TPMS_WHEELTAG_TEMP_MIN -128
-#define TPMS_WHEELTAG_TEMP_MAX 127
-#define TPMS_WHEELTAG_TEMP_MASK 0xFF
-#define TPMS_WHEELTAG_TEMP_LSHIFT (TPMS_WHEELTAG_TEMP_BYTE * 8)
-#define TPMS_WHEELTAG_FLAGS_BYTE 2
-#define TPMS_WHEELTAG_FLAGS_LSHIFT (TPMS_WHEELTAG_FLAGS_BYTE * 8)
-#define TPMS_WHEELTAG_FLAGS_ONLINE_BIT 0
-#define TPMS_WHEELTAG_FLAGS_ERROR_BIT 1
-#define TPMS_WHEELTAG_FLAGS_REASON_BIT 2
-
+//! @defgroup reasons Причины отправки данных
+//! @{
 #define TPMS_REASON_BITS 3
 
+#define TPMS_REASON_PERIODIC 0
+#define TPMS_REASON_PRES_UNDER_10 1 //!< PressurePro: 10%, TPMS: 12,5%
+#define TPMS_REASON_PRES_UNDER_20 2 //!< PressurePro: 20%, TPMS: 25%
+#define TPMS_REASON_PRES_UNDER_50 3
+#define TPMS_REASON_OVERTEMP 4
+#define TPMS_REASON_PRES_FAST_DEC 5
+#define TPMS_REASON_NEWAIR_OVERPRES 6 //!< PressurePro: new air, TPMS: overpressure
+#define TPMS_REASON_NEWMAGNET 7
+
+#define TPMS_REASON_MIN TPMS_REASON_PERIODIC
+#define TPMS_REASON_MAX TPMS_REASON_NEWMAGNET
+//! @}
+
+//! @defgroup pressure Давление
+//! @{
 #define TPMS_PRESSURE_MIN 0
 #define TPMS_PRESSURE_MAX 255
+//! @}
 
 #define TPMS_WHEEL_DATA [ \
     .temperature,\
@@ -44,30 +41,18 @@
     .reason,\
 ]
 
-#define TPMS_COMTAG_WHEEL_PRES_BYTE 0
-#define TPMS_COMTAG_WHEEL_PRES_SIZE 1
-#define TPMS_COMTAG_WHEEL_TEMPFLAGS_BYTE (TPMS_COMTAG_WHEEL_PRES_BYTE + TPMS_COMTAG_WHEEL_PRES_SIZE)
-#define TPMS_COMTAG_WHEEL_TEMPFLAGS_SIZE 1
-#define TPMS_COMTAG_WHEEL_TEMP_BIT 0
-#define TPMS_COMTAG_WHEEL_TEMP_BITS 3
-#define TPMS_COMTAG_WHEEL_TEMP_MASK 0xFF
-#define TPMS_COMTAG_TEMP_MIN -40
-#define TPMS_COMTAG_TEMP_MAX 100
-#define TPMS_COMTAG_TEMP_STEP ((TPMS_COMTAG_TEMP_MAX - TPMS_COMTAG_TEMP_MIN) / ((1 << TPMS_COMTAG_WHEEL_TEMP_BITS) - 1))
-#define TPMS_COMTAG_WHEEL_ONLINE_BIT (TPMS_COMTAG_WHEEL_TEMP_BIT + TPMS_COMTAG_WHEEL_TEMP_BITS)
-#define TPMS_COMTAG_WHEEL_ERROR_BIT TPMS_COMTAG_WHEEL_ONLINE_BIT + 1
-#define TPMS_COMTAG_WHEEL_REASON_BIT TPMS_COMTAG_WHEEL_ERROR_BIT + 1
-#define TPMS_COMTAG_WHEEL_INIT_VALUE ((TPMS_REASON_PERIODIC << TPMS_COMTAG_WHEEL_REASON_BIT) \
-                                    + (1 << TPMS_COMTAG_WHEEL_ERROR_BIT) \
-                                    + (1 << TPMS_COMTAG_WHEEL_ONLINE_BIT))
-#define TPMS_COMTAG_WHEEL_SIZE (TPMS_COMTAG_WHEEL_PRES_SIZE + TPMS_COMTAG_WHEEL_TEMPFLAGS_SIZE)
-#define TPMS_COMTAG_WHEELS_SIZE (TPMS_WHEELS_MAX * TPMS_COMTAG_WHEEL_SIZE)
-#define TPMS_COMTAG_SEND_FLAG_POS (TAG_TPMS_SIZE - 1)
+//! @brief Инициализировать параметры шин
+//! @param[out] wheels массив структур шин
+forward stock tpmsInitWheels(wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA]);
 
+//! @brief Записать параметры шин в общий тег
+//! @param[in] wheels массив структур шин
+forward stock tpmsWriteCommonTag(const wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA]);
 
-tpmsWriteCommonTag(const wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA]);
-tpmsClearCommonTag();
-tpmsWriteWheelTag(wheelIdx, const wheel[TPMS_WHEEL_DATA]);
-tpmsInitWheels(wheels[TPMS_WHEELS_MAX][TPMS_WHEEL_DATA]);
+//! @brief Очистить общий тег шин
+forward stock tpmsClearCommonTag();
 
-#endif
+//! @brief Записать в тег колесного датчика состояние шины
+//! @param[in] wheelIdx индекс шины TPMS_WHEEL_INDEX_MIN..TPMS_WHEEL_INDEX_MAX
+//! @param[in] wheel структура данных шины
+forward stock tpmsWriteWheelTag(wheelIdx, const wheel[TPMS_WHEEL_DATA]);
